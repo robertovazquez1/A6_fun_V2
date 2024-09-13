d251bd84cc9d0334e59f3b5758682fccda15df67
Roberto Vazquez <a.22308051281326@cbtis128.edu.mx>
2024-09-13 10:04:43 -0600

    Add files via upload

---
+++ funcionesv2.py
@@ -0,0 +1,41 @@
+print("Funciones V2")
+print("Roberto Vazquez ")
+celulares=["Samsung a52", "iPhone 11", "Chafa"]
+v3=("Kokichi", "Himiko", "Shuichi")
+incredibox={"Alpha", "Brazil", "Alive"}
+banda = {
+    "A7X": 5,
+    "LP": 5,
+    "3DG": 4,
+}
+
+#Zona de Funciones
+def tel(telefonos):
+    for uncelular in telefonos:
+        print(uncelular)
+
+def LIII(personajes):
+    for victim in personajes:
+        print(victim) 
+
+def beat(box):
+    for polo in box:
+        print(polo)
+
+def muse(rock):
+    for guitar in rock:
+        print(guitar) 
+
+def mbanda(b):
+    for en in b:
+        print(en) 
+
+# Llamadas a Funciones
+print("Imrpimir Celulares")
+tel(celulares)
+print("Imrpimir Personajes")
+LIII(v3)
+print("Imrpimir Versiones")
+beat(incredibox)
+print("Imrpimir Bandas")
+mbanda(banda)
